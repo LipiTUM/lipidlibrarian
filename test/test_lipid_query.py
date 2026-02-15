bab99066fc46df0dc75f599666db7869873b7947
Felix Niedermaier <felix.niedermaier@googlemail.com>
2026-02-15 17:37:38 +0100

    working on more tests for merging code

--- test/test_lipid_query.py
+++ test/test_lipid_query.py
@@ -39,7 +39,86 @@ def test_lipidquery_id(sample_ids):
         result = q.query()
         assert result
 
+
 def test_lipid_merge_raises_on_wrong_type():
     a = Lipid()
     with pytest.raises(ValueError):
         a.merge(object())
+
+
+def test_merge_keeps_exact_level_only():
+    """
+    If lipids exist at the query level -> keep only those.
+    """
+    q = LipidQuery("PC 18:0_20:1", method="name")
+
+    lipid_1 = Lipid()
+    lipid_1.nomenclature.name = 'PC 18:0_20:1'
+    lipid_2 = Lipid()
+    lipid_2.nomenclature.name = 'PC 18:1(9Z)/20:0'
+    lipid_3 = Lipid()
+    lipid_3.nomenclature.name = 'PC 38:1'
+
+    q.lipids = [lipid_1, lipid_2, lipid_3]
+    q.merge_lipids()
+
+    assert len(q.lipids) == 1
+    assert q.lipids[0].nomenclature.level == Level.molecular_lipid_species
+    assert q.lipids[0].nomenclature.get_name() == "PC 18:0_20:1"
+
+
+def test_merge_keeps_all_higher_if_no_exact():
+    """
+    If no lipids at the query level, but there are higher -> keep all higher level lipids and merge lower ones into them.
+    """
+    q = LipidQuery('PC 18:0_20:0', method="name")
+
+    lipid_1 = Lipid()
+    lipid_1.nomenclature.name = 'PC 18:0/20:1'
+    lipid_2 = Lipid()
+    lipid_2.nomenclature.name = 'PC 18:1(9Z)/20:0'
+    lipid_3 = Lipid()
+    lipid_3.nomenclature.name = 'PC 38:1'
+
+    q.lipids = [lipid_1, lipid_2, lipid_3]
+    q.merge_lipids()
+
+    assert {l.nomenclature.level for l in q.lipids} == {Level.structural_lipid_species, Level.isomeric_lipid_species}
+    assert {l.nomenclature.get_name() for l in q.lipids} == {"PC 18:0/20:1", "PC 18:1(9Z)/20:0"}
+
+
+def test_merge_keeps_all_if_no_exact_or_higher():
+    """
+    If no lipids at the query level and no higher level lipids exist -> keep all lipids.
+    """
+    q = LipidQuery("PC 18:0/20:0", method="name")
+
+    lipid_1 = Lipid()
+    lipid_1.nomenclature.name = "PC 34:1"
+    lipid_2 = Lipid()
+    lipid_2.nomenclature.name = "PC 18:1_20:0"
+
+    q.lipids = [lipid_1, lipid_2]
+    q.merge_lipids()
+
+    assert len(q.lipids) == 2
+    assert {l.nomenclature.level for l in q.lipids} == {Level.sum_lipid_species, Level.molecular_lipid_species}
+
+
+def test_merge_deduplicates_within_kept_level():
+    """
+    Within the kept set, identical lipids are merged/deduplicated using Lipid.merge (same level + same nomenclature name).
+    """
+    q = LipidQuery("PC 18:1_20:0", method="name")
+
+    lipid_1 = Lipid()
+    lipid_1.nomenclature.name = "PC 18:1_20:0"
+    lipid_2 = Lipid()
+    lipid_2.nomenclature.name = "PC 18:1_20:0"
+
+    q.lipids = [lipid_1, lipid_2]
+    q.merge_lipids()
+
+    assert len(q.lipids) == 1
+    assert q.lipids[0].nomenclature.level == Level.molecular_lipid_species
+    assert q.lipids[0].nomenclature.get_name() == "PC 18:1_20:0"
